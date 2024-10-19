0b25167946ea4e63cef288941f4828155bcba87f
LeidiFlores <floresreyesleidi@gmail.com>
2024-10-19 10:29:23 -0600

    change the base url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/cuchi-pomodoro/',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
