5cf53b4fa2f97ad48c24a39920217b381f049bda
Natnicha Lerdwanawattana <n.lerdwa@gmail.com>
2024-10-20 23:20:27 -0400

    vue.js setup for dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,25 @@
 <script setup>
 import PomodoroTimer from './components/PomodoroTimer.vue'
+// importing onMounted from vue
+import { ref, onMounted } from 'vue'
+
+// reactive state for light/dark mode
+const isDarkMode = ref(localStorage.getItem('mode') === 'dark')
+
+// function to toggle light/dark mode
+const toggleMode = () => {
+  isDarkMode.value = !isDarkMode.value
+  document.body.classList.toggle('dark-mode', isDarkMode.value)
+  localStorage.setItem('mode', isDarkMode.value ? 'dark' : 'light')
+}
+
+// on mount
+onMounted(() => {
+  if (isDarkMode.value) {
+    document.body.classList.add('dark-mode');
+  }
+});
+
 </script>
 
 <template>
